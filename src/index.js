6d42c893ebdf5fdbad8a12b2de95b6b6c0230993
Priscila <pritorquato@gmail.com>
2020-08-06 14:51:21 -0300

    Completando aula 3

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import{BrowserRouter , Switch, Route} from 'react-router-dom';
 import CadastroVideo from './pages/cadastro/Video';
 import CadastroCategoria from './pages/cadastro/Categoria';
 
+
+// Desafio master blaster na descrição
+// Colocar um jogo nessa página: https://www.youtube.com/watch?v=jOAU81jdi-c :)
+const Pagina404 = () => (<div>Página 404</div>)
+
 ReactDOM.render(
   // <React.StrictMode>
   //   <App />
@@ -16,7 +21,7 @@ ReactDOM.render(
     <Route path="/" component={Home} exact /> 
     <Route path = "/cadastro/video" component ={CadastroVideo}  />
     <Route path = "/cadastro/categoria" component ={CadastroCategoria}  />
-    <Route path="/" component={() => (<div>Página 404</div>)} />      
+    <Route component={Pagina404} />      
   </Switch>
 
 </BrowserRouter>,
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,21 +1,102 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria(){
-    return(
-      <PageDefault>
-        <h1>Cadastro de Categoria</h1>
-
-
-
-
-
-        <Link to ="/">
-            Ir para home
-        </Link>
-      </PageDefault>
-    )
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
   }
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
+ 
+  
+  function setValue(chave, valor){
+    setValues({
+      ...values, 
+      [chave]: valor, //nome: 'valor'
+    })
+  }
+
+  function handlerChange(infosDoEvento){
+    setValue(
+      infosDoEvento.target.getAttribute('name'),
+      infosDoEvento.target.value
+    );
+  }
+
+  return(
+    <PageDefault>
+      <h1>Cadastro de Categoria: {values.nome}</h1>
+
+      <form onSubmit={function handleSubmit(infosDoEvento){
+        infosDoEvento.preventDefault();
+        setCategorias( [
+          ...categorias,
+          values
+          ] );
+
+          setValues(valoresIniciais)
+        }}>
+        
+        <FormField
+          label = "Nome da Categoria"
+          type="text"
+          name="nome"     
+          value={values.nome}
+          onChange = {handlerChange}
+        />
+      
+
+{/*         <div>
+          <label>
+            Descrição: 
+            <textarea
+              type="text" 
+              value={values.descricao}
+              name="descricao"
+              onChange = {handlerChange}
+            />
+          </label>
+        </div> */}
+        
+        <FormField
+          label = "Descrição"
+          type="text"
+          name="descricao"     
+          value={values.descricao}
+          onChange = {handlerChange}
+        />
+
+        <FormField
+          label = "Cor"
+          type="color"
+          name="cor"     
+          value={values.cor}
+          onChange = {handlerChange}
+        />
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+      <ul>
+        {categorias.map( (categoria, indice) => {
+          return(
+            <li key={`${categoria}${indice}`}>
+              {categoria.nome}
+            </li>
+          )
+        } ) }
+      </ul>
+
+      <Link to ="/">
+          Ir para home
+      </Link>
+    </PageDefault>
+  )
+}
 
   export default CadastroCategoria;
